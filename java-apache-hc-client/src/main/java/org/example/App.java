b5bfbe4395d7d061bed83bbb3209f6056a613f97
LeiYangGH <675686066@qq.com>
2021-06-25 10:43:20 +0800

    useApacheHc() function

--- java-apache-hc-client/src/main/java/org/example/App.java
+++ java-apache-hc-client/src/main/java/org/example/App.java
@@ -11,9 +11,10 @@ import org.apache.http.impl.client.HttpClients;
 
 public class App {
 
-  public static void main(String[] args) throws IOException {
+  private static void useApacheHc() throws IOException {
     CloseableHttpClient httpclient = HttpClients.createDefault();
-    HttpGet httpGet = new HttpGet("http://127.0.0.1:8080");
+//    HttpGet httpGet = new HttpGet("http://127.0.0.1:8080");
+    HttpGet httpGet = new HttpGet("http://10.71.20.181:8080");
     CloseableHttpResponse response = httpclient.execute(httpGet);
     try {
       HttpEntity responseEntity = response.getEntity();
@@ -43,5 +44,10 @@ public class App {
     } finally {
       response.close();
     }
+  }
+
+
+  public static void main(String[] args) throws IOException {
+    useApacheHc();
   }
 }
